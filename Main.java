707427c314997458f96ec43d2dca22dfbb9a2501
brookeslight <46109714+brookeslight@users.noreply.github.com>
2021-03-16 16:08:18 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,12 @@
 package main;
 
-import java.util.Arrays;
-
 public class Main {
 
 	public static void main(String[] args) {
-		NeuralNetwork nn = new NeuralNetwork(new int[] {2,3,2});
+		NeuralNetwork nn = new NeuralNetwork(2,3,2);
 //		System.out.println("Prediction:");
 //		System.out.println(Arrays.deepToString(nn.predict(new double[][] {{1},{2}})));
 		nn.train(new double[][] {{1},{2}}, new double[][] {{1},{0}});
-		nn.showState();
-		nn.train(new double[][] {{4},{8}}, new double[][] {{1},{0}});
 		nn.showState();
 	}
 
--- NeuralNetwork.java
+++ NeuralNetwork.java
@@ -12,13 +12,14 @@ public class NeuralNetwork {
 	private ArrayList<double[][]> weightGradients;
 	private ArrayList<double[][]> biasGradients;
 	
-	public NeuralNetwork(int[] identity) {
+	public NeuralNetwork(int... identity) {
 		this.n = identity.length;
 		this.weights = new ArrayList<double[][]>();
 		this.biases = new ArrayList<double[][]>();
 		this.activations = new ArrayList<double[][]>();
 		this.weightGradients = new ArrayList<double[][]>();
 		this.biasGradients = new ArrayList<double[][]>();
+		//
 		for(int i = 1; i < this.n; i++) {
 			this.weights.add(this.randomVector(identity[i], identity[i-1]));
 		}
@@ -34,10 +35,14 @@ public class NeuralNetwork {
 		for(int i = 1; i < this.n; i++) {
 			this.biasGradients.add(new double[identity[i]][1]);
 		}
+		//
 		this.showState();
 	}
 	
 	public double[][] predict(double[][] input) {
+        if(input.length != this.activations.get(0).length || input[0].length != this.activations.get(0)[0].length) {
+            throw new IllegalArgumentException("Prediction Error!");
+        }
 		this.activations.set(0, input);
 		for(int i = 1; i < this.activations.size(); i++) {
 			this.activations.set(i, this.sigmoid(this.add(this.multiply(this.weights.get(i-1), this.activations.get(i-1)), this.biases.get(i-1))));
@@ -46,6 +51,7 @@ public class NeuralNetwork {
 	}
 	
 	public void train(double[][] input, double[][] target) {
+		//calculate activations
 		this.predict(input);
 		//calculate weight gradients
 		for(int l = 0; l < this.weightGradients.size(); l++) {
@@ -72,55 +78,61 @@ public class NeuralNetwork {
 		}
 	}
 	
-	private double gradientOfWeight(int l, int i, int j, double[][] t) { //when referring to A, use l+1
-		if((l+1) < (this.n-1)) {
-			double sum = 0;
-			for(int k = 0; k < this.weights.get(l+1).length; k++) {
-				sum += this.gradientOfWeight(l+1, k, i, t)*this.weights.get(l+1)[k][i];
+	private double gradientOfWeight(int l, int i, int j, double[][] t) { //when referring to A, use l+1 because A[0] is input vector, n-1 because n starts at 1
+		double z = (this.activations.get(l + 1)[i][0] * (1.0 - this.activations.get(l + 1)[i][0]) * this.activations.get(l)[j][0]);
+		if((l + 1) < (this.n - 1)) {
+			double sum = 0.0;
+			for(int k = 0; k < this.weights.get(l + 1).length; k++) {
+				sum += this.gradientOfWeight(l + 1, k, i, t)*this.weights.get(l + 1)[k][i];
 			}
-			return sum*(this.activations.get(l+1)[i][0]*(1-this.activations.get(l+1)[i][0]))*this.activations.get(l)[j][0]*(1.0f/this.activations.get(l+1)[i][0]);
-		}else if((l+1) == (this.n-1)) {
-			return 2*(this.activations.get(l+1)[i][0]-t[i][0])*(this.activations.get(l+1)[i][0]*(1-this.activations.get(l+1)[i][0]))*this.activations.get(l)[j][0];
+			return ((z * sum) / this.activations.get(l + 1)[i][0]);
+		} else if((l + 1) == (this.n - 1)) {
+			return 2.0 * (this.activations.get(l + 1)[i][0] - t[i][0]) * z;
 		}
-		System.out.println("ERROR!");
-		return 0;
+		throw new IllegalArgumentException("Weight Gradient Calculation Error!");
 	}
 	
-	private double gradientOfBias(int l, int i, int j, double[][] t) { ////when referring to A, use l+1
-		return this.gradientOfWeight(l, i, j, t)*(1.0f/this.activations.get(l)[j][0]);
+	private double gradientOfBias(int l, int i, int j, double[][] t) { //when referring to A, use l+1 because A[0] is input vector, n-1 because n starts at 1
+		double z = (this.activations.get(l + 1)[i][0] * (1.0 - this.activations.get(l + 1)[i][0]));
+		if((l + 1) < (this.n - 1)) {
+			double sum = 0.0;
+			for(int k = 0; k < this.weights.get(l + 1).length; k++) {
+				sum += this.gradientOfWeight(l + 1, k, i, t)*this.weights.get(l + 1)[k][i];
+			}
+			return ((z * sum) / this.activations.get(l + 1)[i][0]);
+		} else if((l + 1) == (this.n - 1)) {
+			return 2.0 * (this.activations.get(l + 1)[i][0] - t[i][0]) * z;
+		}
+		throw new IllegalArgumentException("Bias Gradient Calculation Error!");
 	}
 	
 	private double sigma(double x) {
-		return (1 / (1 + Math.exp(-x)));
+		return (1.0/(1.0 + Math.exp(-x)));
 	}
 	
 	private double normalDistribution(double x) {
-		return (Math.exp(-(x*x)/2) / Math.sqrt(2*Math.PI));
+		return (Math.exp(-(x*x)/2.0)/Math.sqrt(2*Math.PI));
 	}
 	
 	private double[][] randomVector(int row, int col) {
 		double[][] vector = new double[row][col];
 		for(int i = 0; i < row; i++) {
 			for(int j = 0; j < col; j++) {
-				vector[i][j] = this.normalDistribution(-4+Math.random()*8);
+				vector[i][j] = this.normalDistribution(-4 + 8*Math.random());
 			}
 		}
 		return vector;
 	}
 	
 	private double[][] multiply(double[][] A, double[][] B) {
-		int rowA = A.length;
-		int colA = A[0].length;
-		int rowB = B.length;
-		int colB = B[0].length;	
-        if(colA != rowB) {
-            throw new IllegalArgumentException("A:Rows: " + colA + " did not match B:Columns " + rowB + ".");
+        if(A[0].length != B.length) {
+            throw new IllegalArgumentException("Multiplication Error!");
         } 
-        double[][] C = new double[rowA][colB];
-        for(int i = 0; i < rowA; i++) {
-        	for(int j = 0; j < colB; j++) {
-        		for(int k = 0; k < colA; k++) {
-        			C[i][j] += A[i][k] * B[k][j];
+        double[][] C = new double[A.length][B[0].length];
+        for(int i = 0; i < A.length; i++) {
+        	for(int j = 0; j < B[0].length; j++) {
+        		for(int k = 0; k < A[0].length; k++) {
+        			C[i][j] += (A[i][k] * B[k][j]);
         		}
         	}
         }
@@ -128,11 +140,9 @@ public class NeuralNetwork {
 	}
 	
 	private double[][] sigmoid(double[][] A) {
-		int rowA = A.length;
-		int colA = A[0].length;
-        double[][] C = new double[rowA][colA];
-        for(int i = 0; i < rowA; i++) {
-        	for(int j = 0; j < colA; j++) {
+        double[][] C = new double[A.length][A[0].length];
+        for(int i = 0; i < A.length; i++) {
+        	for(int j = 0; j < A[0].length; j++) {
         		C[i][j] = this.sigma(A[i][j]);
         	}
         }
@@ -140,16 +150,12 @@ public class NeuralNetwork {
 	}
 	
 	private double[][] add(double[][] A, double[][] B) {
-		int rowA = A.length;
-		int colA = A[0].length;
-		int rowB = B.length;
-		int colB = B[0].length;	
-        if(colA != colB || rowA != rowB) {
-            throw new IllegalArgumentException("no match error");
+        if(A.length != B.length || A[0].length != B[0].length) {
+            throw new IllegalArgumentException("Addition Error!");
         } 
-        double[][] C = new double[rowA][colA];
-        for(int i = 0; i < rowA; i++) {
-        	for(int j = 0; j < colA; j++) {
+        double[][] C = new double[A.length][A[0].length];
+        for(int i = 0; i < A.length; i++) {
+        	for(int j = 0; j < A[0].length; j++) {
         		C[i][j] = (A[i][j] + B[i][j]);
         	}
         }
@@ -157,16 +163,12 @@ public class NeuralNetwork {
 	}
 	
 	private double[][] subtract(double[][] A, double[][] B) {
-		int rowA = A.length;
-		int colA = A[0].length;
-		int rowB = B.length;
-		int colB = B[0].length;	
-        if(colA != colB || rowA != rowB) {
-            throw new IllegalArgumentException("no match error");
+        if(A.length != B.length || A[0].length != B[0].length) {
+            throw new IllegalArgumentException("Subtraction Error!");
         } 
-        double[][] C = new double[rowA][colA];
-        for(int i = 0; i < rowA; i++) {
-        	for(int j = 0; j < colA; j++) {
+        double[][] C = new double[A.length][A[0].length];
+        for(int i = 0; i < A.length; i++) {
+        	for(int j = 0; j < A[0].length; j++) {
         		C[i][j] = (A[i][j] - LEARNINGRATE*B[i][j]);
         	}
         }
